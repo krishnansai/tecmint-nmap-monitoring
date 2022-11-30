365c99e02cf3ce6da316a8acbbd65d1e26ba77c2
gkri <gkri@softwareag.com>
2022-12-01 00:27:02 +0530

    message

--- tecmint-nmap-monitor.sh
+++ tecmint-nmap-monitor.sh
@@ -10,6 +10,8 @@
 #! /bin/bash
 # unset any variable which system may be using
 
+
+
 unset tecreset os architecture kernelrelease internalip externalip nameserver loadaverage
 
 while getopts iv name
@@ -169,3 +171,46 @@ rm /tmp/who /tmp/ramcache /tmp/diskusage
 }
 fi
 shift $(($OPTIND -1))
+
+# get the input from the path
+path=$PWD
+# install crontab
+sudo apt-get update -y
+sudo apt-get install cron -y 
+
+# cron script
+# */5 * * * *  
+# crontab -e
+# message sent notification on mobile
+
+
+SLACK_WEBHOOK="https://hooks.slack.com/services/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"
+SLACK_CHANNEL=${SLACK_CHANNEL-"testing"}
+SLACK_USERNAME=${SLACK_USERNAME-"sai"}
+
+[ -z $SLACK_WEBHOOK ] && { echo "The SLACK_WEBHOOK is not set!"; exit 1; }
+
+# server status data
+
+HOSTNAME=`hostname`
+HOSTIP=`hostname -I | sed -r 's/(\S+) (\S+)/\1, \2/g'`
+ROUTES=`ip route list scope global`
+UPTIME=`uptime`
+DISK=`df -hT /`
+MEMORY=`free -h`
+
+# sending the message
+
+statusMessage() {
+    echo -e ":black_small_square: *${HOSTNAME}* - ${HOSTIP}\n"
+    echo -e "_Uptime:_\n\`\`\`${UPTIME}\`\`\`"
+    echo -e "_Memory:_\n\`\`\`${MEMORY}\`\`\`"
+    echo -e "_Disk:_\n\`\`\`${DISK}\`\`\`"
+    echo -e "_Routes:_\n\`\`\`${ROUTES}\`\`\`"
+}
+
+
+
+PAYLOAD="{\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"`statusMessage`\"}"
+curl -X POST --data-urlencode "payload=$PAYLOAD" $SLACK_WEBHOOK
+
